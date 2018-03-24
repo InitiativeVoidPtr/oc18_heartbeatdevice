86adefaf93e7da4c3c13560b93442a69e80808fe
Vitalij Selynin <vitalijselynin@msn.com>
2018-03-24 23:14:13 +0100

    Removed multi instance parameter

--- Application/SimpleValue.cpp
+++ Application/SimpleValue.cpp
@@ -15,7 +15,7 @@ SimpleValue::SimpleValue()
   M2MObjectInstance* valueInstance = valueObject->create_object_instance();
 
   valueResource = valueInstance->create_dynamic_resource("5527",
-    "AnalogValues", M2MResourceInstance::STRING, true, true);
+    "AnalogValues", M2MResourceInstance::STRING, true);
 
   valueResource->set_operation(M2MBase::GET_ALLOWED);
   valueResource->set_value((uint8_t*)"0", 1);
